06d4e25030ecd517b682d5c6bcd284d608319fd6
Mirwais Farahi <waisfarahi@example.com>
2025-01-30 14:53:10 +0430

    Update

--- src/viz.py
+++ src/viz.py
@@ -69,3 +69,33 @@ def plot_distribution(data, index, cols, values, colors):
 
     # Show the plot
     plt.show()
+
+# Function to calculate percentage
+def calculate_percentage(df, count_column):
+    total = df[count_column].sum()
+    df['Percentage'] = (df[count_column] / total) * 100
+    return df
+
+# Function to plot bar chart with percentage labels
+def plot_bar_chart_with_percentage(data, category_column, count_column, colors):
+    # Calculate the percentage using the provided count column
+    data = calculate_percentage(data, count_column)
+    
+    # Create the horizontal bar plot
+    ax = plt.barh(data[category_column], data['Percentage'], color=colors)
+
+    # Remove border (spines) around the plot
+    for spine in plt.gca().spines.values():
+        spine.set_visible(False)
+
+    # Add percentage labels on bars
+    for bar in ax:
+        width = bar.get_width()
+        plt.text(width + 1, bar.get_y() + bar.get_height()/2, f"{width:.1f}%",
+                 ha='left', va='center', fontsize=12, color='black')
+
+    plt.grid(axis='x', linestyle='-', alpha=0.07)
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
